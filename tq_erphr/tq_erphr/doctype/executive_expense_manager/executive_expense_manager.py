b412221e25fe965ed97ebf8b5b71b0e6e9c2885d
turqosoft <turqosoft@gmail.com>
2026-04-19 18:37:52 +0530

    Changed field names.

--- tq_erphr/tq_erphr/doctype/executive_expense_manager/executive_expense_manager.py
+++ tq_erphr/tq_erphr/doctype/executive_expense_manager/executive_expense_manager.py
@@ -26,8 +26,8 @@ class ExecutiveExpenseManager(Document):
     def add_travel_expensetype_and_rate(self):
     
             hr_settings = frappe.get_single("HR Settings")
-            self.travel_expense_type = hr_settings.travel_expense_type
-            self.rate_per_km = hr_settings.rate_per_km
+            self.travel_expense_type = hr_settings.tq_travel_expense_type
+            self.rate_per_km = hr_settings.tq_rate_per_km
     
     
     
